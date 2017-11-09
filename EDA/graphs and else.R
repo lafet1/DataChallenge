96c05f99763ed1f3fcbc1db0999246236e2b6bb3
johnnyheineken <janhynek@gmail.com>
2017-11-09 15:19:26 +0100

    few plots

--- EDA/graphs and else.R
+++ EDA/graphs and else.R
@@ -1,23 +1,66 @@
-ggplot(data = subdf, aes(x=region_id,y=log(price))) + geom_col()
+ggplot(data = categories, aes(log(avg_price))) + 
+  geom_density(kernel='gaussian', color = "red", fill = 'red', alpha = 0.3) + 
+  theme(plot.subtitle = element_text(vjust = 1), 
+    plot.caption = element_text(vjust = 1), 
+    plot.title = element_text(size = 17, 
+        vjust = 1)) +labs(title = "Density of avg_price in category", 
+    x = "log of average price")
 
 
 
-summary(subdf$price)
+ggplot(data = categories, aes(log(count))) + 
+  geom_density(kernel='gaussian', color = "red", fill = 'red', alpha = 0.3) + 
+  theme(plot.subtitle = element_text(vjust = 1), 
+        plot.caption = element_text(vjust = 1), 
+        plot.title = element_text(size = 17, 
+                                  vjust = 1)) +labs(title = "Density of log(count) in category", 
+                                                    x = "log of count")
+
+ggplot(data = categories_big, aes(was_promoted/count)) + 
+  geom_density(kernel='gaussian', color = "red", fill = 'red', alpha = 0.3) + 
+  theme(plot.subtitle = element_text(vjust = 1), 
+        plot.caption = element_text(vjust = 1), 
+        plot.title = element_text(size = 17, 
+                                  vjust = 1)) +labs(title = "Density of was_promoted count in category", 
+                                                    x = "share of was_promoted")
+
 
 
 ggplot(data=subdf) + geom_qq(aes(sample=price))
 
-expensive_categories <- subdf %>% 
+categories <- subdf %>% 
   select(category_id, price, was_promoted) %>%
   mutate(was_promoted = if_else(was_promoted == "t", 1, 0)) %>%
   group_by(category_id) %>% 
   summarise(count = n(), 
             avg_price = mean(price, na.rm = TRUE), 
-            was_promoted = mean(was_promoted, na.rm = TRUE)) %>% 
-  arrange(desc(avg_price)) %>% filter(!is.na(avg_price))
+            was_promoted = sum(was_promoted, na.rm = TRUE)) %>% 
+  arrange(desc(avg_price)) %>% 
+  filter(!is.na(avg_price))
+  
 
-summary(expensive_categories$count)
-expensive_categories[is.na(expensive_categories$avg_price),]
-cor(log(expensive_categories$avg_price), expensive_categories$was_promoted)
-summary(expensive_categories$was_promoted)
 
+division_quantile <- 0.8
+
+categories_big <- categories %>%
+  filter(count > quantile(count, probs = division_quantile))
+
+categories_small <- categories %>%
+  filter(count < quantile(count, probs = division_quantile))
+
+categories_small %>% 
+  summarise(was_promoted = sum(was_promoted), count = sum(count))
+
+categories_big %>% 
+  summarise(was_promoted = sum(was_promoted), count = sum(count))
+
+
+ggplot(categories) + 
+  
+  
+summary(categories$count)
+categories[is.na(categories$avg_price),]
+cor(log(categories$avg_price), categories$was_promoted)
+summary(categories$was_promoted)
+
+  
