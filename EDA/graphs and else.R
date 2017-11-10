ddbac3d58d87883777ec75e3583d6bb829031bda
johnnyheineken <janhynek@gmail.com>
2017-11-10 09:08:43 +0100

    sth

--- EDA/graphs and else.R
+++ EDA/graphs and else.R
@@ -1,3 +1,14 @@
+categories <- subdf %>% 
+  select(category_id, price, was_promoted) %>%
+  mutate(was_promoted = if_else(was_promoted == "t", 1, 0)) %>%
+  group_by(category_id) %>% 
+  summarise(count = n(), 
+            avg_price = mean(price, na.rm = TRUE), 
+            was_promoted = sum(was_promoted, na.rm = TRUE)) %>% 
+  arrange(desc(avg_price)) %>% 
+  filter(!is.na(avg_price))
+
+
 ggplot(data = categories, aes(log(avg_price))) + 
   geom_density(kernel='gaussian', color = "red", fill = 'red', alpha = 0.3) + 
   theme(plot.subtitle = element_text(vjust = 1), 
@@ -28,15 +39,7 @@ ggplot(data = categories_big, aes(was_promoted/count)) +
 
 ggplot(data=subdf) + geom_qq(aes(sample=price))
 
-categories <- subdf %>% 
-  select(category_id, price, was_promoted) %>%
-  mutate(was_promoted = if_else(was_promoted == "t", 1, 0)) %>%
-  group_by(category_id) %>% 
-  summarise(count = n(), 
-            avg_price = mean(price, na.rm = TRUE), 
-            was_promoted = sum(was_promoted, na.rm = TRUE)) %>% 
-  arrange(desc(avg_price)) %>% 
-  filter(!is.na(avg_price))
+
   
 
 
@@ -54,13 +57,5 @@ categories_small %>%
 categories_big %>% 
   summarise(was_promoted = sum(was_promoted), count = sum(count))
 
-
-ggplot(categories) + 
-  
-  
-summary(categories$count)
-categories[is.na(categories$avg_price),]
-cor(log(categories$avg_price), categories$was_promoted)
-summary(categories$was_promoted)
 
   
