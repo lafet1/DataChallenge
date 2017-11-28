1c8c29f346ce6b77832bf1673f090e8c1891549c
johnnyheineken <janhynek@gmail.com>
2017-11-28 11:43:06 +0100

    functional data preparation for keras

--- Analysis/modelling.R
+++ Analysis/modelling.R
@@ -7,41 +7,49 @@ library(datetime)
 
 set.seed(921021)
 
-
+# setwd("~/Škola/UvA/Výuka/Machine Learning for Econometrics/Project/DataChallenge/Analysis")
 d <- read_feather("file_notext.feather")
 d$district_id <- NULL
 d$has_gg <- NULL
 d$has_skype <- NULL
 d$has_phone <- NULL
 d$private_business <- NULL
+d$id <- NULL
 d$created_at_first <- ymd_hms(d$created_at_first, tz = "Asia/Manila")
+d$is_liquid <- as.numeric(ifelse(d$is_liquid == "t", 1, 0))
+d$was_promoted <- as.numeric(ifelse(d$was_promoted == "t", 1, 0))
+
 
 d <- d %>% 
-  mutate(wday = wday(created_at_first, label = TRUE)) %>% 
-  mutate(no_price = as.factor(is.na(price))) %>% 
+  mutate(wday = wday(created_at_first)) %>% 
+  mutate(no_price = as.numeric(is.na(price))) %>% 
   mutate(price = if_else(is.na(price), 0, price)) %>% 
-  mutate(hour = as.factor(hour(created_at_first))) %>% 
-  mutate(no_descr = as.factor(is.na(upper_descr))) %>% 
-  mutate(region_id = as.factor(region_id)) %>%
-  mutate(subregion_id = as.factor(subregion_id)) %>%
-  mutate(category_id = as.factor(category_id)) 
+  mutate(hour = as.numeric(hour(created_at_first))) %>% 
+  mutate(no_descr = as.numeric(is.na(upper_descr))) %>% 
+  mutate(region_id = as.numeric(region_id)) %>%
+  mutate(subregion_id = as.numeric(subregion_id)) %>%
+  mutate(category_id = as.numeric(category_id)) %>% 
+  mutate(month = month(created_at_first))
 
 d[is.na(d)] <- 0
 
 d <- d %>%
-  mutate(user_id = as.factor(user_id)) %>%
-  mutate(visible_in_profile = as.factor(visible_in_profile)) %>%
-  mutate(excl_descr = as.factor(excl_descr)) %>%
-  mutate(has_capslock_descr = as.factor(has_capslock_descr)) %>%
-  mutate(excl_title = as.factor(excl_title)) %>% 
-  mutate(has_capslock_title = as.factor(has_capslock_title))
-d$is_liquid <- as.factor(ifelse(d$is_liquid == "t", TRUE, FALSE))
-
+  mutate(user_id = as.numeric(user_id)) %>%
+  mutate(visible_in_profile = as.numeric(visible_in_profile)) %>%
+  mutate(excl_descr = as.numeric(excl_descr)) %>%
+  mutate(has_capslock_descr = as.numeric(has_capslock_descr)) %>%
+  mutate(excl_title = as.numeric(excl_title)) %>% 
+  mutate(has_capslock_title = as.numeric(has_capslock_title))
 
 d <- d %>% 
-  mutate(bata_price = as.factor(if_else((price + 1) %% 10 == 0, TRUE, FALSE))) %>% 
-  mutate(was_promoted = as.factor(was_promoted))
+  group_by(user_id) %>% 
+  mutate(n_user_id = length(user_id))
 
+d <- d %>% 
+  mutate(bata_price = as.numeric(if_else((price + 1) %% 10 == 0, TRUE, FALSE))) %>% 
+  mutate(was_promoted = as.numeric(was_promoted))
+
+d <- ungroup(d)
 
 
 rnd <- runif(n = dim(d)[1])
@@ -51,7 +59,7 @@ d_train <- d[rnd >= 0.15, ]
 rm(d)
 rm(rnd)
 
-# setwd("~/Škola/UvA/Výuka/Machine Learning for Econometrics/Project/DataChallenge/Analysis")
+
 # load("~/Škola/UvA/Výuka/Machine Learning for Econometrics/Project/DataChallenge/Analysis/.RData")
 
 
@@ -60,14 +68,25 @@ y_test <- to_categorical(d_test$was_promoted)
 y_train <- to_categorical(d_train$was_promoted)
 d_test$was_promoted <- NULL
 d_train$was_promoted <- NULL
+d_train$user_id <- NULL
+d_train$created_at_first <- NULL
 
+d_test$user_id <- NULL
+d_test$created_at_first <- NULL
+d_test$wday <- as.numeric(d_test$wday)
 
+names_d_train <- colnames(d_train)
+d_train <- unname(d_train)
+d_train <- as.matrix(d_train)
 
-
+names_d_test <- colnames(d_test)
+d_test <- unname(d_test)
+d_train <- as.matrix(d_train)
+d_test <- as.matrix(d_test)
 
 model <- keras_model_sequential()
 model %>% 
-  layer_dense(units = 2, activation = 'sigmoid', input_shape = c(106))
+  layer_dense(units = 2, activation = 'sigmoid', input_shape = c(105))
 
 model %>% compile(
   loss = 'categorical_crossentropy',
@@ -76,15 +95,30 @@ model %>% compile(
 )
 
 
+
+
+
 history <- model %>% fit(
   d_train, y_train, 
-  epochs = 30, batch_size = 128, 
+  epochs = 5, batch_size = 128, 
   validation_split = 0.2
 )
 
+sapply(d_train, class)
+sum(sapply(d_train, function(x) sum(is.na(x))))
 
 
+model %>% predict_classes(d_test) %>% head()
+model %>% evaluate(d_test, y_test)
 
+# 630998/630998 [==============================] - 14s 22us/step
+# $loss
+# [1] NaN
+# 
+# $acc
+# [1] 0.9781552
 
+sum(y_test[,1])/dim(y_test)[1]
+# [1] 0.9781552
 
 
