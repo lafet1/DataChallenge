155ae3a3bbaf8c4cc48c9b1bce55e4c38974e381
lafet1 <32439234+lafet1@users.noreply.github.com>
2017-12-03 01:32:14 +0100

    markdown skeleton, modeling changes

--- Analysis/modelling.R
+++ Analysis/modelling.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(keras)
-library(DMwR)
+library(mlr)
 library(caret)
 
 set.seed(921021)
@@ -26,57 +26,70 @@ rm(rnd)
 # load("~/Škola/UvA/Výuka/Machine Learning for Econometrics/Project/DataChallenge/Analysis/.RData")
 
 
-
 y_test <- to_categorical(d_test$was_promoted)
 y_train <- to_categorical(d_train$was_promoted)
 d_test$was_promoted <- NULL
 d_train$was_promoted <- NULL
 
+# balancing data set
+df_train <- cbind(d_train, unname(y_train[, 2]))
+names(df_train)[length(names(df_train))] <- "was_promoted"
+df_train$was_promoted <- as.factor(df_train$was_promoted)
+task = makeClassifTask(data = df_train, target = "was_promoted")
+task.under = undersample(task, rate = 1/3)
+task.over = oversample(task.under, rate = 10)
 
-d_train <- as.matrix(d_train)
+df_train <- getTaskData(task.over)
+y2_train <- to_categorical(as.numeric(df_train$was_promoted) - 1)
+df_train$was_promoted <- NULL
+
+df_train <- as.matrix(df_train)
 d_test <- as.matrix(d_test)
 
-# SMOTE
-full_train <- as.data.frame(cbind(y_train[, 2], d_train))
-names(full_train)[1] <- "target"
-full_train$target <- as.factor(full_train$target)
 
-smote <- SMOTE(target ~ ., full_train, perc.over = 500, perc.under = 300)
-y_smote <- to_categorical(as.numeric(smote$target) - 1)
-x_smote <- as.matrix(smote[, -1])
-
-r <- runif(n = dim(d_train)[1])
-
+# keras
 model <- keras_model_sequential()
 model %>% 
-  layer_dense(units = 64, activation = 'relu', input_shape = c(dim(x_smote)[2]))  %>% 
-  #layer_dropout(rate = 0.25) %>%
-  layer_dense(units = 16, activation = 'relu') %>%
-  #layer_dropout(rate = 0.2) %>%
-  layer_dense(units = 16, activation = 'sigmoid') %>%
-  #layer_dropout(rate = 0.2) %>%
+  layer_dense(units = 512, input_shape = c(dim(df_train)[2])) %>% 
+  layer_dropout(rate = 0.4) %>% 
+  layer_activity_regularization(l1 = 0.00001) %>%
+  layer_activation_leaky_relu(alpha = 0.55) %>%
+  
+  # sofar (probably) best combinations (all in layer 1) - alpha = 0.55, l1 = 0.00003;
+  # alpha = 0.6, l1 = 0.00003; alpha = 0.6, l1 = 0.00002; alpha = 0.55, l1 = 0.00001
+  
+  layer_dense(units = 128) %>%
+  layer_dropout(rate = 0.25) %>%
+  layer_activation_leaky_relu(alpha = 0.3) %>%
+
+  layer_dense(units = 32) %>%
+  layer_dropout(rate = 0.25) %>%
+  layer_activation_leaky_relu(alpha = 0.3) %>%
+  
   layer_dense(units = 2, activation = 'softmax')
+
 
 model %>% compile(
   loss = c('binary_crossentropy'),
-  optimizer = optimizer_adam(lr = 0.0002),
-  metrics = c('accuracy')
-)
-
+  optimizer = optimizer_adam(lr = 0.00001),
+  metrics = c('accuracy'))
 
 
 
 history <- model %>% fit(
-  x_smote, y_smote, shuffle = T,
-  epochs = 5, batch_size = 64, 
-  validation_data = list(d_train[r < 0.1, ], y_train[r < 0.1, ])
-)
+  df_train, y2_train, shuffle = T,
+  epochs = 10, batch_size = 512, 
+  validation_set = 0.2)
 
 
-model %>% 
-  predict_classes(d_test) %>% head()
+pred <- model %>% 
+  predict_classes(d_test)
 
-model %>% evaluate(d_test, y_test)
+# model %>% evaluate(d_test, y_test)
+
+confusionMatrix(as.numeric(pred), y_test[, 2])
+
+#### STOP ####
 
 # sum(y_test[, 1]) / sum(y_test) -- 0.978083
 
