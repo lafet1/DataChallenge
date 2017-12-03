eeff6bd1c9e10dc1c05e8c80e1d5ed43ecabf7f8
lafet1 <32439234+lafet1@users.noreply.github.com>
2017-12-03 16:20:44 +0100

    final version of NN

--- Analysis/modelling.R
+++ Analysis/modelling.R
@@ -52,15 +52,16 @@ model <- keras_model_sequential()
 model %>% 
   layer_dense(units = 512, input_shape = c(dim(df_train)[2])) %>% 
   layer_dropout(rate = 0.4) %>% 
-  layer_activity_regularization(l1 = 0.00001) %>%
-  layer_activation_leaky_relu(alpha = 0.55) %>%
+  layer_activity_regularization(l1 = 0.00002) %>%
+  layer_activation_leaky_relu(alpha = 0.35) %>%
   
   # sofar (probably) best combinations (all in layer 1) - alpha = 0.55, l1 = 0.00003;
   # alpha = 0.6, l1 = 0.00003; alpha = 0.6, l1 = 0.00002; alpha = 0.55, l1 = 0.00001
   
-  layer_dense(units = 128) %>%
-  layer_dropout(rate = 0.25) %>%
-  layer_activation_leaky_relu(alpha = 0.3) %>%
+  layer_dense(units = 256) %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_activity_regularization(l1 = 0.0000145) %>%
+  layer_activation_leaky_relu(alpha = 0.5) %>%
 
   layer_dense(units = 32) %>%
   layer_dropout(rate = 0.25) %>%
