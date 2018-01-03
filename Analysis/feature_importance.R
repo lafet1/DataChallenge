f6e3f8c1b9c41cd2717fa51efb897e4cc537b152
lafet1 <32439234+lafet1@users.noreply.github.com>
2018-01-03 10:11:51 +0100

    feature importance
    i have some minor change, want to push it

--- Analysis/feature_importance.R
+++ Analysis/feature_importance.R
@@ -5,6 +5,7 @@ cor <- as.numeric(cor(y_train[, 2], d_train[, c(-1:-8)]))
 cor_matrix <- data.frame(names(d_train[, c(-1:-8)]), cor)
 cor_matrix <- cor_matrix[order(cor_matrix[, 2], decreasing = T), ]
 
+write.csv(cor_matrix, "corr.csv")
 
 
 
